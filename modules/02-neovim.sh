c8400f9283b61f0e39cd224cf82f4aece6b35da0
Daniel Newman <daniel.newman@publicinterestco.com>
2026-03-06 16:12:49 -0500

    update neovim setup

--- modules/02-neovim.sh
+++ modules/02-neovim.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
-# Install neovim
+# Install latest neovim from GitHub releases
 
-sudo add-apt-repository -y ppa:neovim-ppa/stable
-sudo apt-get update -y
-sudo apt-get install -y neovim
+NVIM_URL=$(curl -s https://api.github.com/repos/neovim/neovim/releases/latest \
+  | grep "browser_download_url.*nvim-linux-x86_64.tar.gz\"" \
+  | cut -d '"' -f 4)
+
+curl -L -o /tmp/nvim-linux-x86_64.tar.gz "$NVIM_URL"
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf /tmp/nvim-linux-x86_64.tar.gz
+rm /tmp/nvim-linux-x86_64.tar.gz
+
+# Symlink nvim binary
+sudo ln -sf /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
 
 # Point vi and vim to neovim
-sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --install /usr/bin/vi vi /usr/local/bin/nvim 60
+sudo update-alternatives --install /usr/bin/vim vim /usr/local/bin/nvim 60
